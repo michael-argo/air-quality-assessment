d0bc176132d13ecc7440591ee89fd4e480983e9d
margo <michaelargo13@gmail.com>
2021-08-31 16:16:01 -0400

    some cleanup. removed US only limitation

--- air-quality-assessment/src/App.js
+++ air-quality-assessment/src/App.js
@@ -3,21 +3,16 @@ import "semantic-ui-css/semantic.min.css";
 
 import {
   Button,
-  Container,
   Divider,
   Dropdown,
-  Form,
   Grid,
   Header,
   Icon,
-  List,
-  Search,
   Segment,
 } from "semantic-ui-react";
 import React, { useEffect, useState } from "react";
 
 import axios from "axios";
-import logo from "./logo.svg";
 
 const fetchUrl = "https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2";
 
@@ -28,12 +23,18 @@ const App = () => {
   const [fetchResults, setFetchResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
 
-  const numCities = 2;
+  useEffect(() => {
+    const asyncCheckSession = async () => {
+      await axios
+        .get(`${fetchUrl}/cities?sort=asc&order_by=city&limit=100000`)
+        .then((e) => initCityDropdown(e))
+        .catch((err) => setErrorMessage(err));
+    };
 
-  console.log("fetch results are", fetchResults);
+    asyncCheckSession();
+  }, []);
 
   const initCityDropdown = (serverResponse) => {
-    console.log("callint it", serverResponse);
     const data = serverResponse?.data?.results;
     setCityOptions(
       data.map((item) => {
@@ -44,7 +45,6 @@ const App = () => {
 
   const handleCompare = async () => {
     //check duplicates and dispaly error if comparing the same city. can be improved by just removing a selected city as an option from other dropdowns
-
     const selectedCitiesSet = new Set(Object.values(selectedCities));
     if (Object.values(selectedCities).length !== selectedCitiesSet.size) {
       setErrorMessage("Please select cities of different names");
@@ -54,12 +54,10 @@ const App = () => {
       setErrorMessage(null);
     }
 
-    const cityFetchUrl = `${fetchUrl}/measurements?limit=1&country_id=US&city=`;
-    console.log("city options are", selectedCities);
+    const cityFetchUrl = `${fetchUrl}/measurements?limit=1&city=`;
     const fetchRequests = Object.keys(selectedCities).map((opt) => {
       return axios.get(`${cityFetchUrl}${selectedCities[opt]}`);
     });
-    console.log("fetch requests are", fetchRequests);
     await axios
       .all(fetchRequests)
       .then((e) => setFetchResults(e.map((resp) => resp?.data?.results)))
@@ -74,22 +72,6 @@ const App = () => {
       </div>
     </div>
   );
-
-  useEffect(() => {
-    const asyncCheckSession = async () => {
-      //u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/cities?sort=asc&country_id=US&order_by=city
-      await axios
-        .get(
-          `${fetchUrl}/cities?sort=asc&country_id=US&order_by=city&limit=100000`
-        )
-        .then((e) => initCityDropdown(e))
-        .catch((err) => setErrorMessage(err));
-    };
-
-    asyncCheckSession();
-  }, []);
-
-  console.log("fetch results are", fetchResults);
 
   const cityComponent = (cityNumber) => {
     return (
