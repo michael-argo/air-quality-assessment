4c0779de9527143548b32ddd884d01b648c1a9d6
margo <michaelargo13@gmail.com>
2021-09-01 11:45:14 -0400

    added a parameter filter to aid in comparison

--- air-quality-assessment/src/App.js
+++ air-quality-assessment/src/App.js
@@ -22,6 +22,8 @@ const App = () => {
   const [buttonDisabled, setButtonDisabled] = useState(true);
   const [fetchResults, setFetchResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [parameterOptions, setParameterOptions] = useState([]);
+  const [chosenParameter, setChosenParameter] = useState("");
 
   useEffect(() => {
     const asyncCheckSession = async () => {
@@ -30,6 +32,11 @@ const App = () => {
           `${fetchUrl}/cities?sort=asc&order_by=city&limit=100000&country=US`
         )
         .then((e) => initCityDropdown(e))
+        .catch((err) => setErrorMessage(err));
+
+      await axios
+        .get(`${fetchUrl}/parameters?limit=100&sort=asc`)
+        .then((e) => initParameterOptions(e))
         .catch((err) => setErrorMessage(err));
     };
 
@@ -45,32 +52,64 @@ const App = () => {
     );
   };
 
+  const initParameterOptions = (serverResponse) => {
+    const data = serverResponse?.data?.results;
+    const uniqueElements = {};
+    data.forEach((item) => {
+      if (!uniqueElements[item.name]) {
+        uniqueElements[item.name] = item.displayName;
+      }
+    });
+    setParameterOptions(
+      Object.keys(uniqueElements).map((item) => {
+        return { key: item, value: item, text: uniqueElements[item] };
+      })
+    );
+  };
+
   const handleCompare = async () => {
     //check duplicates and dispaly error if comparing the same city. can be improved by just removing a selected city as an option from other dropdowns
+    setFetchResults([]);
     const selectedCitiesSet = new Set(Object.values(selectedCities));
     if (Object.values(selectedCities).length !== selectedCitiesSet.size) {
       setErrorMessage("Please select cities of different names");
-      setFetchResults([]);
       return;
     } else {
       setErrorMessage(null);
     }
 
-    const cityFetchUrl = `${fetchUrl}/measurements?limit=1&country=US&city=`;
+    const cityFetchUrl = `${fetchUrl}/measurements?limit=1&country=US&parameter=${chosenParameter}&city=`;
     const fetchRequests = Object.keys(selectedCities).map((opt) => {
       return axios.get(`${cityFetchUrl}${selectedCities[opt]}`);
     });
     await axios
       .all(fetchRequests)
-      .then((e) => setFetchResults(e.map((resp) => resp?.data?.results)))
+      .then((e) => {
+        //axios all maintains order of requests in response body
+        const result = [];
+        e.forEach((resp, index) => {
+          if (resp?.data?.results?.length < 1) {
+            setErrorMessage(
+              `City ${
+                index + 1
+              } does not have data for parameter ${chosenParameter}. Please select another parameter or city for comparison.`
+            );
+          } else {
+            result.push(resp.data.results);
+          }
+        });
+        if (result.length === e.length) {
+          setFetchResults(result);
+        }
+      })
       .catch((err) => setErrorMessage(err));
   };
 
   const resultDisplay = (cityInfo) => (
-    <div class="content">
-      <h3 class="header">{cityInfo?.city}</h3>
-      <div class="meta">
-        <span>{`${cityInfo?.value} ${cityInfo?.unit}`}</span>
+    <div className="content">
+      <h3 className="header">{cityInfo?.city}</h3>
+      <div className="meta">
+        <span>{`${cityInfo?.value} ${cityInfo?.unit} (parameter: ${cityInfo?.parameter})`}</span>
       </div>
     </div>
   );
@@ -94,7 +133,9 @@ const App = () => {
           onChange={(e, data) => {
             const newCities = { ...selectedCities, [cityNumber]: data.value };
             setSelectedCities(newCities);
-            if (Object.values(newCities).length > 1) setButtonDisabled(false);
+            if (Object.values(newCities).length > 1 && chosenParameter !== "") {
+              setButtonDisabled(false);
+            }
           }}
         />
         {fetchResults.length > 0 &&
@@ -118,15 +159,27 @@ const App = () => {
   return (
     <div className="App">
       <Header as="h1">Compare Air Assessment Between Two Cities</Header>
+      <Dropdown
+        placeholder="Select Parameter for Comparison"
+        fluid
+        selection
+        options={parameterOptions}
+        onChange={(e, data) => {
+          if (Object.values(selectedCities).length > 1) {
+            setButtonDisabled(false);
+          }
+          setChosenParameter(data.value);
+        }}
+      />
       <SegmentExamplePlaceholderGrid />
 
       <Button primary disabled={buttonDisabled} onClick={handleCompare}>
         Compare
       </Button>
       {errorMessage && (
-        <div class="ui negative message transition">
-          <i class="close icon" onClick={() => setErrorMessage(null)}></i>
-          <div class="header">{errorMessage}</div>
+        <div className="ui negative message transition">
+          <i className="close icon" onClick={() => setErrorMessage(null)}></i>
+          <div className="header">{errorMessage}</div>
         </div>
       )}
     </div>
